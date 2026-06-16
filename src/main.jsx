23af62d86d2bbceb4cccaf420fb604e5f6ea4590
education-upgrade <christilley1987@gmail.com>
2026-06-16 19:02:18 +0100

    Load reference layout overrides

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './styles.css'
 import './plannerExtras.css'
+import './referenceLayout.css'
 
 const rootElement = globalThis.document.getElementById('root')
 createRoot(rootElement).render(<App />)
