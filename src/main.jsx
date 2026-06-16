3ace04e218facebb238a23015ac8a88e894c680f
education-upgrade <christilley1987@gmail.com>
2026-06-16 16:37:39 +0100

    Load extra planner styles

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './styles.css'
+import './plannerExtras.css'
 
 const rootElement = globalThis.document.getElementById('root')
 createRoot(rootElement).render(<App />)
