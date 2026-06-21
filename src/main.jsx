70891add70e3c327cbc44b44cf373ceee37c80c4
education-upgrade <christilley1987@gmail.com>
2026-06-21 09:02:42 +0100

    Load second new recipe batch

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './newRecipeBatch1.js'
+import './newRecipeBatch2.js'
 import App from './App.jsx'
 import './styles.css'
 import './plannerExtras.css'
